233b033eea322d435c494a93ce02bed98e8663bf
pentryyy <m.v.volikov@gmail.com>
2026-08-20 02:26:55 +0300

    fixed undo operation

--- src/main/kotlin/com/pentryyy/prettylogjson/actions/FormatLogAction.kt
+++ src/main/kotlin/com/pentryyy/prettylogjson/actions/FormatLogAction.kt
@@ -5,9 +5,10 @@ import com.google.gson.JsonParser
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.SelectionModel
+import com.intellij.openapi.project.Project
 
 class FormatLogAction : AnAction() {
 
@@ -15,7 +16,7 @@ class FormatLogAction : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
         val editor: Editor = e.getData(CommonDataKeys.EDITOR) ?: return
-
+        val project = e.project
         val selectionModel: SelectionModel = editor.selectionModel
         val selectedText: String? = selectionModel.selectedText
 
@@ -31,7 +32,7 @@ class FormatLogAction : AnAction() {
         val formattedText = resultLines.joinToString("\n")
         val isSelection = !selectedText.isNullOrBlank()
 
-        replaceTextInEditor(editor, selectionModel, formattedText, isSelection)
+        replaceTextInEditor(editor, selectionModel, formattedText, isSelection, project)
     }
 
     private fun extractJsonFromLine(line: String): String? {
@@ -59,16 +60,22 @@ class FormatLogAction : AnAction() {
         editor: Editor,
         selectionModel: SelectionModel,
         formattedText: String,
-        isSelection: Boolean
+        isSelection: Boolean,
+        project: Project?
     ) {
-        ApplicationManager.getApplication().runWriteAction {
-            if (isSelection) {
-                val start = selectionModel.selectionStart
-                val end = selectionModel.selectionEnd
-                editor.document.replaceString(start, end, formattedText)
-            } else {
-                editor.document.setText(formattedText)
+        WriteCommandAction.runWriteCommandAction(
+            project,
+            "Pretty Log JSON",
+            null,
+            {
+                if (isSelection) {
+                    val start = selectionModel.selectionStart
+                    val end = selectionModel.selectionEnd
+                    editor.document.replaceString(start, end, formattedText)
+                } else {
+                    editor.document.setText(formattedText)
+                }
             }
-        }
+        )
     }
 }
